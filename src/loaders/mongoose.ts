6f34e75de3088a069f7bf9ffdc670bd0df4ab627
Kelvin Nketia-Achiampong <achiampongk22@gmail.com>
2021-11-30 01:36:57 +0000

    logging config.databaseURL

--- src/loaders/mongoose.ts
+++ src/loaders/mongoose.ts
@@ -3,6 +3,7 @@ import { Db } from 'mongodb';
 import config from '../config';
 
 export default async (): Promise<Db> => {
+  console.log(config.databaseURL)
   const connection = await mongoose.connect(config.databaseURL, {
     useNewUrlParser: true,
     useCreateIndex: true,
